c605f827aa31183fa9a1d67bbd7b1fd1d97bf966
milan.zivanov <milan.zivanov@gmail.com>
2018-06-21 13:06:06 +0200

    Directives HostListiner HostBinding ElementRef final 2

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,17 +1,17 @@
 export class Recipe {
-  // public name: string;
-  // public description: string;
-  // public imagePath: string;
+  public name: string;
+  public description: string;
+  public imagePath: string;
 
-  // // long way
-  // constructor(name: string, desc: string, imagePath: string) {
-  //   this.name = name;
-  //   this.imagePath = imagePath;
-  //   this.description = desc;
-  // }
+  // long way
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.imagePath = imagePath;
+    this.description = desc;
+  }
 
   // short way
-  constructor(private name: string,
-              private description: string,
-              private imagePath: string) {}
+  // constructor(private name: string,
+  //             private description: string,
+  //             private imagePath: string) {}
 }
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -3,6 +3,22 @@ import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core'
 @Directive({
   selector: '[appDropdown]'
 })
+
+
+export class DropdownDirective {
+
+  constructor(private _el: ElementRef) {}
+
+  @HostBinding('class.show') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+      this._el.nativeElement.querySelector('.dropdown-menu').classList.toggle('show');
+
+  }
+
+}
+
+// course way
 // export class DropdownDirective {
 //   @HostBinding('class.show') isOpen = false;
 
@@ -10,31 +26,3 @@ import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core'
 //     this.isOpen = !this.isOpen;
 //   }
 // }
-
-
-export class DropdownDirective {
-
-  // private isOpen = false;
-  constructor(private _el: ElementRef) {}
-
-  @HostBinding('class.show') isOpen = false;
-
-  @HostListener('click') toggleOpen() {
-      // this.isOpen = true;
-      // this.isOpen = !this.isOpen;
-      this._el.nativeElement.querySelector('.dropdown-menu').classList.toggle('show');
-
-  }
-  // @HostListener('document:click', ['$event.target']) close (targetElement) {
-  //     const inside: boolean = this._el.nativeElement.contains(targetElement);
-  //     if (!inside) {
-  //         this.isOpen = false;
-  //         this._el.nativeElement.querySelector('.dropdown-menu').classList.remove('show');
-  //     }
-  // }
-
-  // @HostListener('click') toggleOpen() {
-  //   this.isOpen = !this.isOpen;
-  // }
-
-}
