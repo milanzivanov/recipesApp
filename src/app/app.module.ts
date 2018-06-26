3fef4aafbe2de925a358a4fb5144c04c857a2590
milan.zivanov <milan.zivanov@gmail.com>
2018-06-26 18:36:08 +0200

    Setting up Routs, Adding to the app and Active Routs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BasicDirective } from './shared/basic.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 
