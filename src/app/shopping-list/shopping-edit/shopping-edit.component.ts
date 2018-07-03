dbdfa1e09a1b68a71d6f35b0a6af58365801b54b
milan.zivanov <milan.zivanov@gmail.com>
2018-07-03 12:56:53 +0200

    206 Resetting the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,7 +37,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
 
@@ -46,7 +46,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
-
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
