2d643c11e9608d484e71e8d149c9c03db6639114
milan.zivanov <milan.zivanov@gmail.com>
2018-07-08 18:54:52 +0200

    245 GETting Back the Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,4 +19,8 @@ export class HeaderComponent {
         );
   }
 
+  onFatchData() {
+    this.dataStorageService.getRecipes();
+  }
+
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
+import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class DataStorageService {
@@ -12,5 +13,15 @@ export class DataStorageService {
   storeRecipes() {
     return this.http
       .put('https://ng-recipe-book-ac1ff.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-ac1ff.firebaseio.com/recipes.json')
+        .subscribe(
+          (response: Response) => {
+            const recipes: Recipe[] = response.json();
+            this.recipeService.setRecipes(recipes);
+          }
+        );
   }
 }
