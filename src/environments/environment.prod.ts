0640320ac764c70a6cd7cc5b26472a0272ceea6c
milan.zivanov <milan.zivanov@gmail.com>
2018-07-13 13:33:13 +0200

    281 How to use AoT Compilation with the CLI-subtitle-en app routing do not belongs to this commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyALb8FF8at_FKdyZhNZIDNgnYcDNF1Xzqk',
+    authDomain: 'ng-recipe-book-ac1ff.firebaseapp.com',
+    databaseURL: 'https://ng-recipe-book-ac1ff.firebaseio.com',
+    projectId: 'ng-recipe-book-ac1ff',
+    storageBucket: 'ng-recipe-book-ac1ff.appspot.com',
+    messagingSenderId: '877635911291'
+  }
 };
